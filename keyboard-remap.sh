1e8453649dcd04eea37d20f45a67f24ef5acd035
Alexander Sosedkin <asosedkin@redhat.com>
2022-07-22 00:32:27 +0200

    keyboard-remap.sh: relax onenetbook 4 wildcard

--- keyboard-remap.sh
+++ keyboard-remap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 KBD=/dev/input/by-path/platform-i8042-serio-0-event-kbd
-ONENETBOOK4=/sys/devices/pci0000:00/0000:00:15.0/i2c_designware.0/i2c-*/i2c-SYNA3602:00/0018:0911:5288.0007
+ONENETBOOK4=/sys/devices/pci0000:00/0000:00:15.0/i2c_designware.0/i2c-*/i2c-SYNA3602:00/0018:0911:5288.000*
 X1_CARBONG9=/sys/devices/pci0000:00/0000:00:15.0/i2c_designware.0/i2c-*/i2c-ELAN0672:00/0018:04F3:3187.0001
 
 if [ -e $KBD ] && [ -e $ONENETBOOK4 ]; then exec keyboard-remap-onemix; fi
